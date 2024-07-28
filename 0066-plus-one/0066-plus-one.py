285884749d1c4bd3dc7506b12747f1b403b7ba76
HarshaJamedar <harsha_jemedar@csu.fullerton.edu>
2024-07-27 22:30:20 -0700

    Time: 17 ms (47.31%), Space: 11.6 MB (26.43%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        for i in range(len(digits)-1,-1,-1):
-            if digits[i] == 9:
-                digits[i] = 0
-            else:
-                digits[i] = digits[i]+1
+        for i in reversed(range(len(digits))):
+
+            if digits[i] != 9:
+                digits[i] += 1
                 return digits
-        return [1] + digits
+            digits[i] = 0
+        return [1] + digits   
 
 
         
