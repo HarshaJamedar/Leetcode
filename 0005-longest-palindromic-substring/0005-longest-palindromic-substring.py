f07d5ec6102c6dacfd6fb5e2b1db01db3503f21b
HarshaJamedar <harsha_jemedar@csu.fullerton.edu>
2024-11-07 10:42:23 -0700

    Time: 235 ms (81.57%), Space: 16.6 MB (83.78%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,26 +1,19 @@
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        def check(l,r):
-            #checking for outofbounds and have same characters around the centre
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand(l,r):
             while l>=0 and r<len(s) and s[l] == s[r]:
                 l -= 1
                 r += 1
-            #returning the palindrome between l and r by slicing
             return s[l+1:r]
 
-        longest_palindrome = ""
+        result = ""
         for i in range(len(s)):
-            substring1 = check(i,i)
-            if len(substring1) > len(longest_palindrome):
-                longest_palindrome = substring1
-            #when the length of substring is even need to increment i 
-            substring2 = check(i,i+1)
-            if len(substring2) > len(longest_palindrome):
-                longest_palindrome = substring2
-        return longest_palindrome
-
+            substring1 = expand(i,i)
+            if len(substring1) > len(result):
+                result = substring1
+            substring2 = expand(i, i+1)
+            if len(substring2) > len(result):
+                result = substring2
+        return result
+        
         
